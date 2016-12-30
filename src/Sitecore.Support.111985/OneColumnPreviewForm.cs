b668ce548330514b5fe14985e2470a4e82697333
Andrey Filchenkov <afi@sitecore.net>
2016-12-30 18:39:08 +0200

    Initial

---
+++ src/Sitecore.Support.111985/OneColumnPreviewForm.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Shell;
+using System;
+using System.Collections.Generic;
+
+namespace Sitecore.Support.Shell.Applications.ContentManager.Dialogs.OneColumnPreview
+{
+    public class OneColumnPreviewForm : Sitecore.Shell.Applications.ContentManager.Dialogs.OneColumnPreview.OneColumnPreviewForm
+    {
+        protected override void OnPreRender(EventArgs e)
+        {
+            Sitecore.Security.Accounts.User user = Sitecore.Context.User;
+            Sitecore.Security.UserProfile profile = user.Profile;
+            List<string> keys = profile.GetCustomPropertyNames();
+            string key = "/" + user.Name + "/UserOptions.ContentEditor.RenderCollapsedSections";
+            bool hasCustomerOwnSetting = keys.Contains(key);
+
+            bool renderCollapsedSections = UserOptions.ContentEditor.RenderCollapsedSections;
+            try
+            {
+                UserOptions.ContentEditor.RenderCollapsedSections = true;
+                base.OnPreRender(e);
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception.ToString(), this);
+            }
+            finally
+            {
+                UserOptions.ContentEditor.RenderCollapsedSections = renderCollapsedSections;
+                if (!hasCustomerOwnSetting)
+                {
+                    profile.RemoveCustomProperty(key);
+                    profile.Save();
+                }
+            }
+        }
+    }
+}
+
---
+++ src/Sitecore.Support.111985/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.111985")]
+[assembly: AssemblyProduct("Sitecore.Support.111985")]
+[assembly: ComVisible(false)]
